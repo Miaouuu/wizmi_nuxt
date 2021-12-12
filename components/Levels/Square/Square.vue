db6266e93aa4fd6e0bd2442a5b78bfd6ea24da9a
Guillaume Prigent <guillaume@oss.ventures>
2021-12-12 12:27:38 +0100

    add square computer responsive layout

--- components/Levels/Square/Square.vue
+++ components/Levels/Square/Square.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="wizmi-level-square">
-    square
+  <div class="flex flex-column">
+    <div class="flex flex-row h-100">
+      <div class="wizmi-level-aside">
+        aside
+      </div>
+      <div class="flex flex-column wizmi-level-playable-area">
+        <div class="wizmi-level-timeline">
+          timeline
+        </div>
+        <div class="wizmi-level-game-area">
+          game-area
+          {{ level }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +28,34 @@ export default class Square extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import "../../../assets/css/variables";
+
+// TODO? : Move to global css ?
+.wizmi-level-aside, .wizmi-level-timeline, .wizmi-level-game-area{
+  border: 2px solid $blue;
+  border-radius: 8px;
+}
+.wizmi-level-aside{
+  display: flex;
+  flex-grow: 1;
+
+  width: 25%;
+}
+
+.wizmi-level-playable-area{
+  flex-grow: 1;
+
+  margin-left: 2%;
+
+  .wizmi-level-timeline{
+    height: 20%;
+    margin-bottom: 2%;
+  }
+  .wizmi-level-game-area{
+    display: flex;
+
+    flex-grow: 1;
+  }
+}
 
 </style>
