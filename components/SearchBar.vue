9516ef15fee505e5cb01d694f2b7755010b4e6cf
Guillaume Prigent <guillaume@oss.ventures>
2022-04-25 09:42:42 +0200

    wip

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    search
+  <div class="search-container">
+    <input type="text" class="search-input">
+    <div class="search-box-icon">
+      <img src="../assets/icons/magnifying-glass-solid.svg" alt="search">
+    </div>
   </div>
 </template>
 
@@ -13,6 +16,33 @@ export default class SearchBar extends Vue {
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.search-container{
+  display: flex;
+  align-items: center;
+  width: 16rem;
+  height: 3rem;
+  padding: 0.5rem;
+  border-bottom: 1px solid #fff;
+  .search-input{
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    border: none;
+  }
+  .search-input:focus{
+    outline: none;
+  }
 
+  .serch-box-icon{
+    width: 3rem;
+    height: 3rem;
+    background-color: transparent;
+    border: none;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
