3ce82c88ff0dac4f2c5efefa8e626343ddc04889
Guillaume <guillaume@oss.ventures>
2022-07-25 17:21:03 +0200

    Update aside location notification

--- components/Aside.vue
+++ components/Aside.vue
@@ -3,18 +3,22 @@
     <NuxtLink to="/" class="nav-link border-b-fff">
       <img src="../assets/logo-white.svg" alt="Wizmi" class="logo">
     </NuxtLink>
-    <div class="aside-container">
-      <NuxtLink to="/worlds" class="nav-link">
-        <img src="../assets/icons/globe.svg" alt="Levels page" class="aside-link">
-      </NuxtLink>
-      <NuxtLink to="/levels/1" class="nav-link">
-        <img src="../assets/icons/gaming.svg" alt="Game pages" class="aside-link">
-      </NuxtLink>
-    </div>
-    <div class="aside-footer">
-      <NuxtLink to="/" class="nav-link border-t-fff">
-        <img src="../assets/icons/exit.svg" alt="Exits" class="aside-link">
-      </NuxtLink>
+    <div class="aside-container flex-1">
+      <div class="aside-links">
+        <NuxtLink to="/worlds" class="relative nav-link">
+          <img src="../assets/icons/globe.svg" alt="Levels page" class="aside-link">
+          <span v-if="$route.path === '/worlds'" class="blue-dot" />
+        </NuxtLink>
+        <NuxtLink to="/levels/1" class="relative nav-link">
+          <img src="../assets/icons/gaming.svg" alt="Game pages" class="aside-link">
+          <span v-if="$route.path.includes('/levels/')" class="blue-dot" />
+        </NuxtLink>
+      </div>
+      <div class="aside-footer">
+        <NuxtLink to="/" class="nav-link border-t-fff">
+          <img src="../assets/icons/exit.svg" alt="Exits" class="aside-link">
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
@@ -26,10 +30,10 @@ export default class Aside extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/css/variables";
 .aside-body {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   padding: 0.5em;
   border-radius: 16px;
   background: #F2B138;
@@ -39,9 +43,15 @@ export default class Aside extends Vue {}
 .aside-container {
   display: flex;
   flex-direction: column;
-  gap: 0.5em;
-  padding-top: 4em;
+  justify-content: space-between;
+  padding-top: 2.5em;
   align-items: center;
+}
+
+.aside-links {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
 }
 
 .aside-footer {
@@ -72,6 +82,17 @@ export default class Aside extends Vue {}
   padding: 12px 0px 16px 0px;
   height: fit-content;
   height: auto;
+}
+
+.blue-dot {
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 2px);
+  background-color: $light-blue;
+  border-radius: 50%;
 }
 
 .nav-link{
